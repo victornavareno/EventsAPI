bc86d1287cd68e5abf12c1cea70033fdd323c11b
victornavareno <vnavareo@alumnos.unex.es>
2025-01-04 11:46:34 +0100

    first commit

---
+++ database_test/init_db.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+# ESTE ARCHIVO ES UN TEST PARA CONECTARSE A LA BASE DE DATOS CON LOS SIGUIENTES CREDENCIALES:
+try:
+    conn = psycopg2.connect(
+        database="Eventos",
+        host="localhost",
+        user="postgres",
+        password="12345",
+        port="5433"
+    )
+    cur = conn.cursor()
+
+    cur = conn.cursor()
+    
+    # cargo todos las tuplas en el cursor
+    cur.execute("SELECT * FROM events;")
+    rows = cur.fetchall()
+    
+    # Imprimo las lineas
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+except psycopg2.Error as e:
+    print("Error connecting to the database:", e)
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Local(db.Model):
+    __tablename__ = 'events'  # table name here
+
+    id_local = db.Column(db.Integer, primary_key=True)  # PRIMARY KEEEY 
+    name = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    events = db.Column(db.ARRAY(db.Integer), nullable=True)
+
+    def to_dict(self):
+        return {
+            'id_local': self.id_local,
+            'name': self.name,
+            'city': self.city,
+            'address': self.address,
+            'events': self.events,
+        }
